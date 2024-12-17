736f10cb64b69462540be9f6948d16537acfc8f2
Lucas Menegatti <menegatti@menegattisp.com.br>
2024-12-17 18:30:37 -0300

    database migrations and configs

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,13 +7,17 @@
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body class="bg-gray-100">
     <x-header />
+    @if(request()->is('/'))
+        <x-hero />
+        <x-top-banner />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
-
     <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
